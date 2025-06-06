6fcf72e81c2d0cb29e7fba14d3d40b299c3de441
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-06-06 18:13:51 -0400

    add a logout button on header

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,20 @@
-const PageHeader = () => {
+import { signOut, auth } from "@/lib/auth"
+import {Button} from "@/components/ui";
+
+const PageHeader = async () => {
+    const session = await auth();
+
     return (
         <header className="">
             <h1 className="audiowide-regular">Next WorldBuilder</h1>
+            {session? <form
+                action={async () => {
+                    "use server"
+                    await signOut()
+                }}
+            >
+                <Button type="submit">Sign Out</Button>
+            </form> : ""}
         </header>
     )
 }
