ab79dae7d61228d23336edeb1dacee65c77459cc
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-06-09 22:03:43 -0400

    redirect '/' route to '/dashboard' if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { useSession } from "@/lib/auth-client"
 
 export default function Home() {
-  // const { data: session } = useSession();
+  const session = useSession();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center p-8 sm:p-16 pb-16 gap-16">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full sm:w-md">
-        {/*{session ? redirect('/dashboard') : <LoggedOutView />}*/}
-          <LoggedOutView />
+        {session ? redirect('/dashboard') : <LoggedOutView />}
       </main>
     </div>
   );
