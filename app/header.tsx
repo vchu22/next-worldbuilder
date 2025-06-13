3cb3ae39dae051ebf80ebd5e67a17e267b6855f4
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-06-12 22:24:37 -0400

    clicking on logo in the header bar leads to the home page

--- app/header.tsx
+++ app/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SignedIn } from "@daveyplate/better-auth-ui";
 import SignOutButton from "@/components/signout-button";
 import { LangDropdownMenu } from "@/components/lang-menu";
@@ -6,7 +7,7 @@ import { DarkModeSwitchButton } from "@/components/dark-mode-switch";
 const PageHeader = () => {
     return (
         <header className="flex flex-row items-center justify-between ml-5">
-            <h1 className="audiowide-regular">Next WorldBuilder</h1>
+            <Link href="/"><h1 className="audiowide-regular">Next WorldBuilder</h1></Link>
 
             <div className="flex flex-row items-center justify-between gap-2">
                 <DarkModeSwitchButton />
